75e7dd3130dd004dc8d60cce1043157596def553
dev126712 <solonoobhacker6767@gmail.com>
2025-11-12 18:17:59 -0500

    .

--- Infrastructure/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc_project.id
-
-  tags = {
-    Name = "VPC project IGW"
-  }
-}
--- Infrastructure/vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "vpc_project" {
-  cidr_block           = var.vpc_cidr
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "vpc-project"
-  }
-}
